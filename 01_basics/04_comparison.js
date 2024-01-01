7c5579862c59aabce08910cafb02075d7b1d7afd
Huzaifa164 <93993472+Huzaifa164@users.noreply.github.com>
2024-01-01 11:44:49 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,8 @@
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+// console.log(null == undefined);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
